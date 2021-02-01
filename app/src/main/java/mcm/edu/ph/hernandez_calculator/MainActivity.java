278bc63dbf64eacdce4cf82e73575636fefa39b4
MonsteroZXC <vladivlad95@gmail.com>
2021-02-01 10:28:17 +0800

    Added buttons in code

--- app/src/main/java/mcm/edu/ph/hernandez_calculator/MainActivity.java
+++ app/src/main/java/mcm/edu/ph/hernandez_calculator/MainActivity.java
@@ -3,6 +3,7 @@ package mcm.edu.ph.hernandez_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btnAdd,btnMinus,btnMultiply,btnDivide,btnDecimal,btnChangeSymbol;
+
+        btn0 = findViewById(R.id.btn0)
+        btn1 = findViewById(R.id.btn1)
+        btn2 = findViewById(R.id.btn2)
+        btn3 = findViewById(R.id.btn3)
+        btn4 = findViewById(R.id.btn4)
+        btn5 = findViewById(R.id.btn5)
+        btn6 = findViewById(R.id.btn6)
+        btn7 = findViewById(R.id.btn7)
+        btn8 = findViewById(R.id.btn8)
+        btn9 = findViewById(R.id.btn9)
+
     }
+
 }
